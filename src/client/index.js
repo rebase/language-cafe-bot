531d90124a10993ed2cbe68e6b54b581d20bbffd
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2026-05-24 10:46:43 +0800

    Add permissions

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,14 @@
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
 
 export default new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMessageReactions,
   ],
+  partials: [Partials.Channel, Partials.Message],
 });
